33587db6ec9dd41c81ee18152991b3fea319677c
Nico Almiron <almiron.nicolas16@gmail.com>
2023-08-16 22:36:34 -0300

    se inicializo el proyecto y se comenzaron a crear los componentes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import "./assets/reset.css";
+
+function App() {
+  return (
+    <Container>
+      <h1>hola</h1>
+    </Container>
+  );
+}
+
+export default App;
